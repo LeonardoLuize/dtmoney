3eb79e3ac68bbff132181b85bd5461755ae5b42b
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-08-30 17:24:35 -0300

    dt money

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModal(false);
+  }
+
   return (
-    <div>
-      <GlobalStyle></GlobalStyle>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal onCloseNewTransactionModal={handleCloseNewTransactionModal} isOpen={isNewTransactionModalOpen} />
+      <GlobalStyle />
+    </>
   );
 }
 
