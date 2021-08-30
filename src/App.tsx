55d9b37e51a72033592242a35f4ee0de2767e612
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-08-30 13:33:40 -0300

    styled components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import './App.css';
+import { GlobalStyle } from "./styles/global";
+
 
 export function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <GlobalStyle></GlobalStyle>
+      <h1>Hello</h1>
+    </div>
   );
 }
 
